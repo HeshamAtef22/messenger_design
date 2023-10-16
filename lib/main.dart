ffd0c09fd6449f178e41cbca1d23115dc55384fe
Hisham Atef <heshamatef040@gmail.com>
2023-10-16 03:24:31 +0300

    create userscreen and use listview with modelclass

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:messenger_design/home_screen.dart';
+//import 'package:messenger_design/home_screen.dart';
+import 'package:messenger_design/users_screen.dart';
 
 void main(){
   runApp(MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      home: UsersScreen(),
     );
   }
 }
